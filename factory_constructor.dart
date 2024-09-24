1fa4c4a01728bbec7b82215adb641affbe068eb0
AqsaBano330 <aqsabano08@gmail.com>
2024-09-24 20:57:22 +0500

    dart classes

--- factory_constructor.dart
+++ factory_constructor.dart
@@ -1,3 +1,30 @@
+
+
 main(){
+
+Student student = Student.now("A@qsa", "dfgh@jkllkjh");
+print(student.email);
+print(student.name );
+
+}
+
+class Student{
+
+String? email;
+String? name;
+Student();
+
+
+factory Student.now(String n, String e){
+  var student = Student();
+  if (e.contains("@")){
+   student.email= e ;
+  }
+  student.name=n;
+  return student;
+  
+
+}
+
 
 }
